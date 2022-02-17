b0e16cd505b4bdab268677e6735423287279a50b
Paulo <git@pauloo27.dev>
2022-02-16 22:52:15 -0300

    feat: add simple cli using Go "flag" package

--- cmd/bf/main.go
+++ cmd/bf/main.go
@@ -1,11 +1,41 @@
-package bf
+package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+
+	"github.com/Pauloo27/bf/pkg/bf"
 )
 
+var (
+	filePath string
+	input    string
+)
+
+func init() {
+	flag.StringVar(&filePath, "file", "", "the program source code file path")
+	flag.StringVar(&input, "input", "", "the program input")
+}
+
 func main() {
-	fmt.Println("wip")
-	os.Exit(1)
+	flag.Parse()
+	if filePath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	rawCode, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("cannot open source file")
+		os.Exit(1)
+	}
+	pgm := bf.NewProgram(string(rawCode), input)
+	out, err := pgm.Run()
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+	for i := 0; i < pgm.OutputIndex; i++ {
+		fmt.Print(string(out[i]))
+	}
 }
